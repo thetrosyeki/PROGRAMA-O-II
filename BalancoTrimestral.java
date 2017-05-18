7a20814203dbcef54866e6288bf06f33388853e2
thetrosyeki <danilo123diogo@gmail.com>
2017-05-18 10:43:51 -0300

    Add files via upload

---
+++ BalancoTrimestral.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author alexandre
+ */
+public class BalancoTrimestral {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x;
+        Scanner input = new Scanner(System.in);
+        x = input.nextInt();
+        System.out.println(x / 0.0);
+    }
+    
+}
